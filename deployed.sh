a7baa2a1e20cfad215468733cbcfbe8e8f18fcb9
MSFee <1360023821@qq.com>
2020-02-22 10:42:47 +0800

    修改配置文件

--- deployed.sh
+++ deployed.sh
@@ -1,12 +1,8 @@
-console.log('开始拉取代码')
 echo "开始拉取代码"
 git pull
-console.log('开始安装依赖')
 echo "开始安装依赖"
 npm i
-console.log('开始打包项目')
 echo "开始打包项目"
 npm run build
-console.log('开始复制文件')
 echo "开始复制文件"
 cp ./build/* ../../www/

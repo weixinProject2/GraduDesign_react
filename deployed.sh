72c7a25d7a3ad0a8e6303a69dd4b37c163e7a132
MSFee <1360023821@qq.com>
2020-02-22 12:11:45 +0800

    最终版本部署命令

--- deployed.sh
+++ deployed.sh
@@ -1,8 +1,17 @@
+# !/bin/bash
+
+WEB_PATH='/root/frontend/GraduDesign_react/'
+
 echo "Start pull"
 git pull
 echo "Start instll development"
 npm i
 echo "Start build file"
 npm run build
-cp ./build/* ../../www/
+echo "delete old file in www file"
+cd /root/www/
+rm -rf *
+cd $WEB_PATH
+echo "copy files"
+cp -r ./build/* ../../www/
 echo "End"

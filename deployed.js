4e7d26549fe800edd124a67b00e86cd9fc089f0c
MSFee <1360023821@qq.com>
2020-02-22 10:35:49 +0800

    测试需改

--- deployed.js
+++ deployed.js
@@ -5,7 +5,7 @@ var handler = createHandler({
   path: '/frontEnd',
   secret: 'woaini'
 })
-// assd
+// assddd
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
     res.statusCode = 404;

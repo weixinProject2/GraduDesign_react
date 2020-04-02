4af4150905e9ac08750c04e40934b8bbb99b3d81
NollieLeo <736653759@qq.com>
2020-04-02 14:09:31 +0800

    [FIX] try to fix auto deployment

---
+++ src/tool-components/FileTypeBlock/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const fileTag = ({ fileType }) => (
+  <div className="gradu-file-tag">
+
+  </div>
+);
+
+export default fileTag;
+
--- src/utils/history.js
+++ src/utils/history.js
@@ -7,4 +7,4 @@ const history = createHashHistory({
 });
 
 
-export default history
+export default history;
